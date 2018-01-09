6736181ba6ba4227059908aed4ee0a80f855f49d
Erik Ovegard <erik.ovegard@seaware.se>
2018-01-09 14:09:12 +0100

    Changed to new project format and added more supported target frameworks

---
+++ PointF.cs
@@ -0,0 +1,36 @@
+﻿using System;
+#if NETSTANDARD1_0
+namespace wContour
+{
+    /// <summary>
+    /// A point in space (replaces System.Drawing.PointF)
+    /// </summary>
+    public struct PointF
+    {
+        /// <summary>
+        /// The X-coordinate
+        /// </summary>
+        public float X { get; }
+
+        /// <summary>
+        /// The Y-coordinate
+        /// </summary>
+        public float Y { get; }
+
+        /// <summary>
+        /// Create a PointF
+        /// </summary>
+        public PointF(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            return $"{nameof(X)}: {X}, {nameof(Y)}: {Y}";
+        }
+    }
+}
+#endif
